681f6648fcfb7ca73e4ad11d8463ee445f0bcd9d
Adrian Khris <teknisi220995@gmail.com>
2025-04-14 08:10:42 +0700

    tambah file untuk olah data

---
+++ data.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<title> Bussiness Analyst Education </title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+		<header>
+			<h2> Bussiness Analyst Education - Ester Veronika Sinaga </h2>
+		</header>
+
+		<nav>
+			<a href="/BAEdu"> Beranda </a>
+			<a href="data.php"> Data </a>
+			<hr><hr> <br>
+		</nav>
+
+		<main>
+			<section>
+				<center>
+					<h3> Input Kuesioner </h3>
+				</center>
+			</section>
+
+			<section>
+				<center>
+					<h3> Data Kuesioner </h3>
+				</center>
+			</section>
+		</main>
+	</body>
+</html>
+
+<style>
+	main{
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+</style>
