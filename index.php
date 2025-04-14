4f3bc5596c2f28e9cf9c2503e4b38f6be465d356
adrian <teknisi220995@gmail.com>
2025-04-14 08:02:49 +0700

    penyesuaian tampilan dasar

--- index.php
+++ index.php
@@ -5,5 +5,26 @@
 	</head>
 
 	<body>
+		<header>
+			<h2> Bussiness Analyst Education - Ester Veronika Sinaga </h2>
+		</header>
+
+		<nav>
+			<a href="/BAEdu"> Beranda </a>
+			<a href="data.php"> Data </a>
+			<hr><hr> <br>
+		</nav>
+
+		<main>
+			<section>
+				<center>
+					<h3> Grafik </h3>
+				</center>
+			</section>
+		</main>
 	</body>
 </html>
+
+<style>
+	
+</style>
